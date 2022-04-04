d7ba767876534118ddabf9795ba85b62dd650c39
jcelisca <celiss1997@gmail.com>
2022-04-04 13:41:25 -0500

    CrossOrigin para la conexión desde el frontend

--- src/main/java/com/sofka/Ferreteria/controller/VentaController.java
+++ src/main/java/com/sofka/Ferreteria/controller/VentaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class VentaController {
 
